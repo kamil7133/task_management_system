f240be573035f42fa7735ff6dc17a2f3c1e34612
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-23 15:02:02 +0100

    initial commit

---
+++ app/models.py
@@ -0,0 +1,9 @@
+from app import db
+
+class Project(db.Model):
+    __tablename__ = 'projects'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.Text, nullable=False)
+
