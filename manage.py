502b02b47e73336e48ce4c8bec286825249a6535
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-30 19:52:12 +0100

    initial update

--- manage.py
+++ manage.py
@@ -1,33 +1,35 @@
+from flask.cli import FlaskGroup
 from app import create_app, db
 from app.models import Project, Sprint, Task
-from datetime import date
-from flask import current_app
 
+# Utwórz aplikację Flask
 app = create_app()
 
-@app.cli.command("seed-data")
+# Utwórz grupę CLI dla aplikacji Flask
+cli = FlaskGroup(app)
+
+@cli.command("seed-data")
 def seed_data():
     with app.app_context():
         if Project.query.count() == 0:
-            example_project = Project(name="Sample Project with Dates", description="Proof of Concept w/ dates")
+            example_project = Project(name="Sample Project", description="Proof of Concept project.")
             db.session.add(example_project)
             db.session.commit()
 
-            sprint_with_dates = Sprint(
-                name="Sprint with Dates",
+            example_sprint = Sprint(
+                name="Sprint 1",
                 project_id=example_project.id,
-                start_date=date(2024, 11, 10),
-                end_date=date(2024, 11, 20),
+                start_date="2024-01-01",
+                end_date="2024-01-15"
             )
-            db.session.add(sprint_with_dates)
+            db.session.add(example_sprint)
             db.session.commit()
 
-            task1 = Task(title="Task A", description="To do", status="To Do", sprint_id=sprint_with_dates.id)
-            task2 = Task(title="Task B", description="In progress", status="In Progress", sprint_id=sprint_with_dates.id)
-            task3 = Task(title="Task C", description="Done", status="Done", sprint_id=sprint_with_dates.id)
+            task1 = Task(title="Task A", description="To do", status="To Do", sprint_id=example_sprint.id)
+            task2 = Task(title="Task B", description="In progress", status="In Progress", sprint_id=example_sprint.id)
+            task3 = Task(title="Task C", description="Done", status="Done", sprint_id=example_sprint.id)
             db.session.add_all([task1, task2, task3])
             db.session.commit()
 
-            print("seeded data")
-        else:
-            print("error seeding data")
+if __name__ == "__main__":
+    cli()
