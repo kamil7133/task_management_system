66f3b90498485e78353e3a305002890edfae0474
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-30 20:23:33 +0100

    initial update

--- wsgi.py
+++ wsgi.py
@@ -1,5 +1,26 @@
-from app import create_app
-from flask import redirect, url_for
+from flask import Flask
+from config import Config
+from .extensions import db, migrate
+from app.models import Project, Sprint, Task
+from flask_sqlalchemy import SQLAlchemy
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    from .app.routes.project_routes import project_bp
+    app.register_blueprint(project_bp)
+
+    from .app.routes.sprint_routes import sprint_bp
+    app.register_blueprint(sprint_bp)
+
+    from .app.routes.task_routes import task_bp
+    app.register_blueprint(task_bp)
+
+
 
 app = create_app()
 
