d34527549b2eaeb97de99f271a45884a18c6ac9c
kamil7133 <piwowarczykkamil23@gmail.com>
2024-12-30 20:05:49 +0100

    initial update

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from flask import redirect, url_for
 
 app = create_app()
 
-@app.route("/")
-def index():
-    return redirect(url_for("project_bp.show_kanban"))
-
 if __name__ == '__main__':
     app.run(debug=False)
 
