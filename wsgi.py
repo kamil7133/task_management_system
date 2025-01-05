69a9a5ebb19add4c429f0a96936b7c09d7db5bf0
kamil7133 <piwowarczykkamil23@gmail.com>
2025-01-05 23:57:22 +0100

    initial update

--- wsgi.py
+++ wsgi.py
@@ -3,9 +3,10 @@ from config import Config
 from app.extensions import db, migrate
 from app.models import Project, Sprint, Task
 from flask_sqlalchemy import SQLAlchemy
+
 from app import create_app
-wgi = create_app()
+app = create_app()
 
 if __name__ == '__main__':
-    wgi.run()
+    app.run()
 
