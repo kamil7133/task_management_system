b9916c9b0651a9c3ecdc816f8011cf13d710e503
kamil7133 <piwowarczykkamil23@gmail.com>
2025-01-05 23:49:45 +0100

    initial update

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
-from config import Config
-from .extensions import db, migrate
-from app.models import Project, Sprint, Task
+from task_management_system.config import Config
+from extensions import db, migrate
+from models import Project, Sprint, Task
 from flask_sqlalchemy import SQLAlchemy
 
 def create_app():
@@ -11,13 +11,13 @@ def create_app():
     db.init_app(app)
     migrate.init_app(app, db)
 
-    from .routes.project_routes import project_bp
+    from routes.project_routes import project_bp
     app.register_blueprint(project_bp)
 
-    from .routes.sprint_routes import sprint_bp
+    from routes.sprint_routes import sprint_bp
     app.register_blueprint(sprint_bp)
 
-    from .routes.task_routes import task_bp
+    from routes.task_routes import task_bp
     app.register_blueprint(task_bp)
 
 
--- wsgi.py
+++ wsgi.py
@@ -4,26 +4,6 @@ from app.extensions import db, migrate
 from app.models import Project, Sprint, Task
 from flask_sqlalchemy import SQLAlchemy
 
-def create_app():
-    wgi = Flask(__name__)
-    wgi.config.from_object(Config)
-
-    db.init_app(wgi)
-    migrate.init_app(wgi, db)
-
-    from app.routes.project_routes import project_bp
-    wgi.register_blueprint(project_bp)
-
-    from app.routes.sprint_routes import sprint_bp
-    wgi.register_blueprint(sprint_bp)
-
-    from app.routes.task_routes import task_bp
-    wgi.register_blueprint(task_bp)
-
-
-
-wgi = create_app()
-
 if __name__ == '__main__':
     wgi.run()
 
